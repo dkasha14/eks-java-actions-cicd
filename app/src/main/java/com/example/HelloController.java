c4f73146974fa65e91b86c39a7a703f3379706c9
dkasha14 <dlrashadk@gmail.com>
2026-03-30 17:39:10 +0000

    Add REST controller with root and health endpoints for web access

---
+++ app/src/main/java/com/example/HelloController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Java App deployed via EKS CI/CD ";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is healthy ✅";
+    }
+}
